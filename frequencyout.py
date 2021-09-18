81444372a1e5c3a13f31209c4ab5553508f7cc78
Igor Korostil <eeghor@gmail.com>
2021-09-18 23:56:25 +1000

    another method

--- frequencyout.py
+++ frequencyout.py
@@ -1,4 +1,7 @@
 import pandas as pd
 from pyod.models.copod import COPOD
+from sklearn.ensemble import IsolationForest
+
+categorical_columns = ['entry_key', 'user_key', 'location_id', 'department_id', 'account_name']
 d = pd.read_csv('data/d_.csv.gz')
 print(d.describe())
