91bf35d35e4543542249f59eabbaa0f93cb2e972
Igor Korostil <eeghor@gmail.com>
2021-09-24 00:12:02 +1000

    more informative output

--- frequencyout.py
+++ frequencyout.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from collections import defaultdict, Counter
+import time
 
 
 class SPAD:
@@ -10,26 +11,26 @@ class SPAD:
 
     def fit(self, X: pd.DataFrame = None):
 
-        self.count_table = {c: Counter(X[c]) for c in X.columns}
+        self.counts = {c: Counter(X[c]) for c in X.columns}
         self.n = len(X)
 
         return self
 
     def predict(self, X):
 
-        spad_scores = np.zeros(shape=(1,))
+        spad_scores = np.zeros(shape=(len(X),))
 
         for c in X.columns:
-            spad_scores = spad_scores + np.log(
+            spad_scores += np.log(
                 X[c]
                 .apply(
-                    lambda x: (self.count_table[c].get(x, 0) + 1)
-                    / (self.n + len(self.count_table[c]))
+                    lambda x: (self.counts[c].get(x, 0) + 1)
+                    / (self.n + len(self.counts[c]))
                 )
                 .values
             )
 
-        return X.assign(spad_score=spad_scores).sort_values("spad_score")
+        return pd.Series(data=spad_scores, name="spad_scores", index=X.index)
 
 
 if __name__ == "__main__":
@@ -41,10 +42,21 @@ if __name__ == "__main__":
         "department_id",
         "account_name",
     ]
-    d = pd.read_csv("data/d_.csv.gz")[categorical_columns]
+
+    data = pd.read_csv("data/d_.csv.gz", usecols=categorical_columns, dtype=str)
+
+    n_train = 1000000
+    n_test = 100000
+
+    X_train = data.sample(n_train)
+    X_test = data.sample(n_test)
+
+    t_start = time.time()
 
     spad = SPAD()
+    spad.fit(X_train)
+    y_pred = spad.predict(X_test)
 
-    spad.fit(X=d.iloc[:1000])
-
-    print(spad.predict(d.sample(400)))
+    print(f"trained on {n_train:,} samples")
+    print(f"predictions on {n_test:,} samples")
+    print(f"elapsed time: {time.time() - t_start: .4f} sec")
