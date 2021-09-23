0f185b1da27eb828f63977fe513ebfe3afece2e4
Igor Korostil <eeghor@gmail.com>
2021-09-23 19:46:44 +1000

    switching to spad

--- frequencyout.py
+++ frequencyout.py
@@ -1,7 +1,25 @@
 import pandas as pd
-from pyod.models.copod import COPOD
-from sklearn.ensemble import IsolationForest
 
-categorical_columns = ['entry_key', 'user_key', 'location_id', 'department_id', 'account_name']
-d = pd.read_csv('data/d_.csv.gz')
-print(d.describe())
+categorical_columns = [
+    "entry_key",
+    "user_key",
+    "location_id",
+    "department_id",
+    "account_name",
+]
+d = pd.read_csv("data/d_.csv.gz")
+
+
+class SPAD:
+    """
+    Simple Probabilistic Method (SPAD)
+    """
+
+    def __init__(self):
+        pass
+
+    def fit(self, X):
+        return self
+
+    def predict(self, X):
+        return X
