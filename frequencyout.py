0c354471ec73114a45acfd77be34c92c5d95ab91
Igor Korostil <eeghor@gmail.com>
2021-09-23 23:27:01 +1000

    first working spad

--- frequencyout.py
+++ frequencyout.py
@@ -1,13 +1,6 @@
 import pandas as pd
-
-categorical_columns = [
-    "entry_key",
-    "user_key",
-    "location_id",
-    "department_id",
-    "account_name",
-]
-d = pd.read_csv("data/d_.csv.gz")
+import numpy as np
+from collections import defaultdict, Counter
 
 
 class SPAD:
@@ -15,11 +8,43 @@ class SPAD:
     Simple Probabilistic Method (SPAD)
     """
 
-    def __init__(self):
-        pass
+    def fit(self, X: pd.DataFrame = None):
 
-    def fit(self, X):
+        self.count_table = {c: Counter(X[c]) for c in X.columns}
+        self.n = len(X)
+
         return self
 
     def predict(self, X):
-        return X
+
+        spad_scores = np.zeros(shape=(1,))
+
+        for c in X.columns:
+            spad_scores = spad_scores + np.log(
+                X[c]
+                .apply(
+                    lambda x: (self.count_table[c].get(x, 0) + 1)
+                    / (self.n + len(self.count_table[c]))
+                )
+                .values
+            )
+
+        return X.assign(spad_score=spad_scores).sort_values("spad_score")
+
+
+if __name__ == "__main__":
+
+    categorical_columns = [
+        "entry_key",
+        "user_key",
+        "location_id",
+        "department_id",
+        "account_name",
+    ]
+    d = pd.read_csv("data/d_.csv.gz")[categorical_columns]
+
+    spad = SPAD()
+
+    spad.fit(X=d.iloc[:1000])
+
+    print(spad.predict(d.sample(400)))
